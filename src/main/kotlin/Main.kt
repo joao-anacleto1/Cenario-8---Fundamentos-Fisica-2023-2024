07691726f9074a5a4d95ab39abdbcd716e29d548
joao-anacleto-a22202260 <joaopranacleto@gmail.com>
2024-01-04 16:39:40 +0000

    add enunciado 8

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,20 @@
-fun main() {
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
 
-    // vou definir um gráfico com 80 caracteres de largura por 25 caracteres de altura
-    val larguraGrafico = 80
-    val alturaGrafico = 25
-    val grafico = Chart(larguraGrafico, alturaGrafico)
+/*
+Enunciado 8:
+Num recinto de jogos radicais um canhão lança um jogador que tem de atingir uma rede vertical de
+segurança que se encontra à distância horizontal de 40m do canhão. A rede tem uma altura de 5m.
+(a) - Determine valores de velocidade inicial e de ângulo de lançamento que permitem que o jogador atinja
+a rede de segurança;
+(b) - Trace a trajétoria do jogador durante o voo.
+O programa deve permitir alterar as condições iniciais do lançamento para poder simular diferentes
+cenários de voo.
+Sugestão: Calcule também o tempo de voo e tempo de subida.
+ */
 
-    println()
-    println("************************************")
-    println("************* Exemplo **************")
-    println("************************************")
-    println()
-
-
-    // vou adicionar 5 pontos ao gráfico
-    // os pontos são representados por dois Double: o x e o y
-    grafico.ponto(1.0, 1.0)
-    grafico.ponto(2.0, 3.0)
-    grafico.ponto(3.0, 5.0)
-    grafico.ponto(4.0, 4.0)
-    grafico.ponto(5.0, 1.0)
-
-    // mando desenhar o gráfico com os pontos previamente introduzidos
-    grafico.draw()
+fun main(){
 
 }
